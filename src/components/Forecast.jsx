47c6152499748b5937421de76ca1596a9e4188f2
yarinmatmoni <yarinmatmoni@gmail.com>
2023-12-07 15:09:17 +0200

    display different icons if exist in favorites list or not

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,9 +1,11 @@
 import FavoriteEmpty from '../assets/svg/favorite-empty-svg.svg';
-// import FavoriteFull from '../assets/svg/favorite-full-svg.svg';
+import FavoriteFull from '../assets/svg/favorite-full-svg.svg';
 import { addToFavorite } from '../store/actions/weather.action';
 import { CardContainer } from './index';
 
-const Forecast = ({ locationData, currentWeather }) => {
+const Forecast = ({ locationData, currentWeather, isEexistInFavorites }) => {
+	isEexistInFavorites(locationData.id);
+
 	const handleOnClick = () => {
 		addToFavorite({ locationData });
 	};
@@ -20,7 +22,12 @@ const Forecast = ({ locationData, currentWeather }) => {
 					<div className='forecast-title'>{currentWeather.text}</div>
 					<div className='temp'>{currentWeather.temp}</div>
 				</div>
-				<img src={FavoriteEmpty} alt='Favorite' onClick={handleOnClick} className='favorite-icon' />
+				<img
+					src={isEexistInFavorites(locationData.id) ? FavoriteFull : FavoriteEmpty}
+					alt='Favorite'
+					onClick={handleOnClick}
+					className='favorite-icon'
+				/>
 			</div>
 			<CardContainer />
 		</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,25 @@ const Home = () => {
 	const [input, setInput] = useState('Tel Aviv');
 	const locationData = useSelector((storeState) => storeState.locationData);
 	const currentWeather = useSelector((storeState) => storeState.currentWeather);
+	const favorites = useSelector((storeState) => storeState.favorites);
 
 	useEffect(() => {
 		loadWeatherData(input);
 	}, []);
 
+	const isEexistInFavorites = (placeId) => {
+		return favorites.some((favorite) => favorite.locationData.id === placeId);
+	};
+
 	return (
 		<div className='page-layout home'>
 			<Search input={input} setInput={setInput} />
 			{currentWeather ? (
-				<Forecast locationData={locationData} currentWeather={currentWeather} />
+				<Forecast
+					locationData={locationData}
+					currentWeather={currentWeather}
+					isEexistInFavorites={isEexistInFavorites}
+				/>
 			) : (
 				<div>Loading...</div>
 			)}
